8c2dc677c4e68e4f35c764f692301daed4be6d21
Marcin <kuzel.marcin@gmail.com>
2018-12-19 21:50:30 +0100

    Get todos and simple test

--- src/main/kotlin/pl/folder/todo/api/TodoEndpoint.kt
+++ src/main/kotlin/pl/folder/todo/api/TodoEndpoint.kt
@@ -2,10 +2,12 @@ package pl.folder.todo.api
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.springframework.http.HttpStatus.CREATED
+import org.springframework.http.HttpStatus.OK
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import pl.folder.todo.api.dto.TodoRequest
@@ -17,11 +19,20 @@ import java.time.Instant
 class TodoEndpoint {
 
     @PostMapping(consumes = ["application/json"])
-    @ResponseStatus(CREATED)
+    @ResponseStatus(OK)
     fun createTodo(@RequestBody todoRequest: TodoRequest): ResponseEntity<Unit> {
         val todo = todoRequest.createTodo()
         logger.info("Todo $todo created")
         return ResponseEntity.ok().build()
+    }
+
+    @GetMapping(produces = ["application/json"])
+    @ResponseBody
+    fun getTodos(): ResponseEntity<List<Todo>> {
+        val todo1 = Todo("test 1", TodoStatus.NEW, Instant.now(), Instant.now())
+        val todo2 = Todo("test 1", TodoStatus.NEW, Instant.now(), Instant.now())
+        val todo3 = Todo("test 1", TodoStatus.NEW, Instant.now(), Instant.now())
+        return ResponseEntity.ok(listOf(todo1, todo2, todo3))
     }
 
     companion object {
