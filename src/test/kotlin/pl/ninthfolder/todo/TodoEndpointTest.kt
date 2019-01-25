8da65a1ce4f268b19f408b55b96c73efb545d3e1
Marcin <kuzel.marcin@gmail.com>
2019-01-25 22:01:27 +0100

    Check todos count in test

--- src/test/kotlin/pl/ninthfolder/todo/TodoEndpointTest.kt
+++ src/test/kotlin/pl/ninthfolder/todo/TodoEndpointTest.kt
@@ -5,16 +5,20 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment
 import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.http.HttpStatus.OK
 import org.springframework.http.ResponseEntity
 import pl.ninthfolder.todo.api.dto.TodoRequest
 import pl.ninthfolder.todo.domain.todo.Todo
+import pl.ninthfolder.todo.domain.todo.TodoRepository
 import pl.ninthfolder.todo.domain.todo.TodoStatus.NEW
+import pl.ninthfolder.todo.infrastructure.persistance.TodoDocumentDao
 import java.time.Instant
 
 @SpringBootTest(webEnvironment= WebEnvironment.RANDOM_PORT)
 class TodoEndpointTest(
-	@Autowired val testRestTemplate: TestRestTemplate
+	@Autowired val testRestTemplate: TestRestTemplate,
+	@Autowired val todoDocumentDao: TodoDocumentDao
 ) {
 
 	@Test
@@ -32,8 +36,8 @@ class TodoEndpointTest(
 		)
 
 		//then
-		response!!
 		assert(response.statusCode == OK)
+		assert(todoDocumentDao.count() == 1L)
 	}
 
 	@Test
